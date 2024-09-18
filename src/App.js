48d19371924aa59da405f0e85bded94eb8669246
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-09-18 15:55:33 +0530

    Seprate into components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import Nav from "./Nav";
 const tempMovieData = [
   {
     imdbID: "tt1375666",
@@ -63,22 +63,7 @@ export default function App() {
 
   return (
     <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
-      </nav>
+      <Nav query={query} setQuery={setQuery} movies={movies} />
 
       <main className="main">
         <div className="box">
---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+export default function Nav({ setQuery, query, movies }) {
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p className="num-results">
+        Found <strong>{movies.length}</strong> results
+      </p>
+    </nav>
+  );
+}
